c9d7855d2d7c0257c56d5cb2a74f9f770bca32ea
SerayaOV <37350853+SerayaOV@users.noreply.github.com>
2020-01-31 16:43:20 +0300

    Update models.py

--- shopapp/models.py
+++ shopapp/models.py
@@ -2,15 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
-#class extra_user_info(models.Model):
- #   usr = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user')
-  #  phone = models.CharField(max_length=100)
-   # address = models.CharField(max_length=100)
-
-    #def __str__(self):
-     #   return self.name
-
 class Product(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_of_product')
     owners = models.ManyToManyField(User)
